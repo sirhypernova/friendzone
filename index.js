4c64052a898cd1092e1d38bad91d585a5f70d53c
SirHyperNova <sirhypernova+gh@gmail.com>
2018-08-10 17:37:44 +0000

    theia test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ClientCore } = require('cmd-dj');
 const { Client } = ClientCore;
 
 const fz = new Client({
-    token: 'NDc0NzYwMjI1NDkzMDkwMzA1.Dkcncg.N0XjZJW51-o8XbL-7E4cGT1l7BI',
+    token: 'NDc0NzYwMjI1NDkzMDkwMzA1.DkkoqA.OH7prpqRkzhLLd3ZFjlIW8YbIYs',
     prefix: 'f!',
     owners: [
         '225738946661974017'
@@ -17,3 +17,5 @@ fz.modules.scan('modules');
 fz.run().then(() => {
    console.log(`Logged in as ${fz.user.tag}`);
 });
+
+// theia test
