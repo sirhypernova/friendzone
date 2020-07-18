a69beb2a7712fce65deff95d1d60b39f518fad17
unlimitedcoder2 <mail@ahowe.dev>
2020-07-18 22:29:50 +0100

    Update index.js
    Fixes the security issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ClientCore } = require('cmd-dj');
 const { Client } = ClientCore;
 
 const fz = new Client({
-    token: 'NDc0NzYwMjI1NDkzMDkwMzA1.DkkoqA.OH7prpqRkzhLLd3ZFjlIW8YbIYs',
+    token: '',
     prefix: 'f!',
     owners: [
         '225738946661974017'
